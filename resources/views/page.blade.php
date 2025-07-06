9d0f9b42e2a8d01f2ccb29c37e89d77c28416bb4
VagharshakGasparyan <vvagharshak@gmail.com>
2025-07-06 23:20:32 +0400

    redesign additional pages

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,30 +1,37 @@
 @extends('root.root')
 @section('title', $page->slug)
 @section('content')
-    <div style="width: 1024px;margin: 25px auto;">
-        @if($page->image)
-            <div style="display: flex; justify-content: center">
-                <img src="{{asset('storage/' . $page->image)}}" alt="image" style="width: 512px; max-width: 100%">
-            </div>
-        @endif
-        @if($page->big_title[app()->getLocale()])
-            <h1>{{$page->big_title[app()->getLocale()]}}</h1>
-        @endif
-        @if($page->medium_title[app()->getLocale()])
-            <h2>{{$page->medium_title[app()->getLocale()]}}</h2>
-        @endif
-        @if($page->small_title[app()->getLocale()])
-            <h3>{{$page->small_title[app()->getLocale()]}}</h3>
-        @endif
-        @if($page->content[app()->getLocale()])
-            <div style="white-space: pre-wrap; text-align: justify">{!! $page->content[app()->getLocale()] !!}</div>
-        @endif
-        <div style="display: flex;flex-wrap: wrap;">
+    <div class="container-wrap">
+        <section class="gallery-section">
+            @if($page->image)
+                <div style="display: flex; justify-content: center">
+                    <img src="{{asset('storage/' . $page->image)}}" alt="image" style="width: 512px; max-width: 100%"  class="gallery-item">
+                </div>
+            @endif
+            @if($page->big_title[app()->getLocale()])
+                <h1>{{$page->big_title[app()->getLocale()]}}</h1>
+            @endif
+            @if($page->medium_title[app()->getLocale()])
+                <h2>{{$page->medium_title[app()->getLocale()]}}</h2>
+            @endif
+            @if($page->small_title[app()->getLocale()])
+                <h3>{{$page->small_title[app()->getLocale()]}}</h3>
+            @endif
+            @if($page->content[app()->getLocale()])
+                <div style="white-space: pre-wrap; text-align: justify">{!! $page->content[app()->getLocale()] !!}</div>
+            @endif
+        </section>
+
+        <div class="gallery">
             @foreach($page->images ?? [] as $imageItem)
-                <div style="width: 250px;height: 250px;
-                background-image: url('{{asset('storage/' . $imageItem)}}'); background-size: cover;margin: 5px;
-                border: 2px solid #ddd;border-radius: 5px;"></div>
+                <img src="{{asset('storage/' . $imageItem)}}" alt="{{$imageItem}}" class="gallery-item">
             @endforeach
+        </div>
+        <div id="slideshow" class="slideshow">
+            <span class="close">&times;</span>
+            <img id="slideshow-img" src="" alt="" />
+            <div class="nav prev">&#10094;</div>
+            <div class="nav next">&#10095;</div>
         </div>
     </div>
 
